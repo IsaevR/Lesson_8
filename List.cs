6c330fb53e41e5b188efed8a7fe82cdce358881c
IsaevR <diplomatic.area@gmail.com>
2018-05-02 08:31:05 +0300

    firstcommit

---
+++ List.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Lesson_8
+{
+    class MyList<T> 
+    {
+        T[] List = new T[0];
+        public int Length { get { return List.Length; } }
+
+
+        public void Add(T value)
+        {
+            T[] newList = new T[List.Length + 1];
+            newList[Length] = value;
+            
+            List = newList;
+        }
+
+        public void Remove(int index)
+        {
+            T[] newList = new T[List.Length - 1];
+            
+            for (int i = 0; i <= List.Length; i++)
+            {
+                newList[i] = List[i];
+                if (index == i)
+                {
+                    continue;
+                }
+                    
+            }
+            List = newList;
+        }
+        public int  Count()
+        {
+            int count = 0;
+            for(int i = 0; i < List.Length; i++)
+            {
+                count++;
+            }
+            return count;
+        }
+    } 
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lesson_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<int[]> arr = new MyList<int[]>();
+            int[] a = new int[] { 10, 20, 30, 40 }; 
+            int[] b = new int[] { 100, 200, 300}; 
+            int[] c = new int[] { 11, 22, 33, 44, 55}; 
+
+            arr.Add(a);
+            arr.Add(b);
+            arr.Add(c);
+                Console.WriteLine("{0}", arr);
+           
+        }
+    }
+}
