c0640f6c711c34258ce04dc32eeeb8cd259630fe
IsaevR <diplomatic.area@gmail.com>
2018-05-12 16:00:51 +0300

    secondcommit

--- List.cs
+++ List.cs
@@ -2,46 +2,64 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
+using System.Collections;
 
 namespace Lesson_8
 {
-    class MyList<T> 
+    class MyList<T> : IEnumerable<T[]>
     {
+        private List<T[]> list = new List<T[]>();
         T[] List = new T[0];
         public int Length { get { return List.Length; } }
 
 
-        public void Add(T value)
-        {
-            T[] newList = new T[List.Length + 1];
-            newList[Length] = value;
-            
-            List = newList;
-        }
+        //public new void Add(T value)
+        //{
+        //    T[] newList = new T[List.Length + 1];
+        //    newList[Length] = value;
 
+        //    List = newList;
+        //}
+        public void Add2(params T[] value)
+        {
+            list.Add(value);
+        }
         public void Remove(int index)
         {
-            T[] newList = new T[List.Length - 1];
+            list.RemoveAt(index);
+            ////T[] newList = new T[List.Length - 1];
             
-            for (int i = 0; i <= List.Length; i++)
-            {
-                newList[i] = List[i];
-                if (index == i)
-                {
-                    continue;
-                }
+            ////for (int i = 0; i <= List.Length; i++)
+            ////{
+            ////    newList[i] = List[i];
+            ////    if (index == i)
+            ////    {
+            ////        continue;
+            ////    }
                     
-            }
-            List = newList;
+            //}
+            //List = newList;
         }
-        public int  Count()
+
+        public IEnumerator<T[]> GetEnumerator()
         {
-            int count = 0;
-            for(int i = 0; i < List.Length; i++)
-            {
-                count++;
-            }
-            return count;
+            return ((IEnumerable<T[]>)list).GetEnumerator();
         }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        public int Count { get { return list.Count; } }
+        //public new int  Count()
+        //{
+        //    int count = 0;
+        //    for (int i = 0; i < List.Length; i++)
+        //    {
+        //        count++;
+        //    }
+        //    return count;
+        //}
     } 
 }
--- Program.cs
+++ Program.cs
@@ -7,16 +7,29 @@ namespace Lesson_8
     {
         static void Main(string[] args)
         {
-            MyList<int[]> arr = new MyList<int[]>();
+            MyList<int> arr = new MyList<int>();
             int[] a = new int[] { 10, 20, 30, 40 }; 
             int[] b = new int[] { 100, 200, 300}; 
             int[] c = new int[] { 11, 22, 33, 44, 55}; 
 
-            arr.Add(a);
-            arr.Add(b);
-            arr.Add(c);
-                Console.WriteLine("{0}", arr);
-           
+            arr.Add2(a);
+            arr.Add2(b);
+            arr.Add2(101, 202, 303, 404, 505);
+
+            foreach (int[] mass in arr)
+            {
+                foreach (var item in mass)
+                {
+                    Console.Write(" {0}",item);
+                }
+                Console.WriteLine();
+            }
+            
+            arr.Remove(2);
+            
+            Console.WriteLine(arr.Count);
+          
+
         }
     }
 }
